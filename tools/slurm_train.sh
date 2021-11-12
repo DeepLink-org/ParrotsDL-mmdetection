9a37a60635a5d908b73dc69177a5d59ed4d07862
wuwenli <wuwenli1@sensetime.com>
2021-11-12 16:42:08 +0800

    fix yaml

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -6,11 +6,19 @@ PARTITION=$1
 JOB_NAME=$2
 CONFIG=$3
 WORK_DIR=$4
-GPUS=${GPUS:-8}
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+GPUS=${GPUS:-1}
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
 CPUS_PER_TASK=${CPUS_PER_TASK:-5}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:5}
+
+#mmcv path
+CONDA_ROOT=/mnt/cache/share/platform/env/miniconda3.6
+MMCV_PATH=${CONDA_ROOT}/envs/${CONDA_DEFAULT_ENV}/mmcvs
+mmcv_version=1.3.12
+export PYTHONPATH=${MMCV_PATH}/${mmcv_version}:$PYTHONPATH
+
+echo $PYTHONPATH
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun -p ${PARTITION} \
