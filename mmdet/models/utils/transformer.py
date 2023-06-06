0211493e1956fe22b6b6dfa0cd4f3501f8a5c266
wanglei9 <wanglei9@sensetime.com>
2023-06-06 20:13:48 +0800

    back to cuda

--- mmdet/models/utils/transformer.py
+++ mmdet/models/utils/transformer.py
@@ -1106,9 +1106,9 @@ class DeformableDetrTransformer(Transformer):
         scale = 2 * math.pi
         dim_t = torch.arange(
             num_pos_feats, dtype=torch.float32, device=proposals.device)
-        dim_t = dim_t.cpu()
+        #dim_t = dim_t.cpu()
         dim_t = temperature**(2 * (dim_t // 2) / num_pos_feats)
-        dim_t = dim_t.cuda()
+        #dim_t = dim_t.cuda()
         # N, L, 4
         proposals = proposals.sigmoid() * scale
         # N, L, 4, 128
